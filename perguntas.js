e027d14b6367f1a0a676e88ef4d47d3dddc02fdb
ErikkunGA <141973843+ErikkunGA@users.noreply.github.com>
2024-10-10 07:57:51 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,30 @@
+criaCartao(
+    'iaovndsuivfdas',
+    'Tetas?',
+    'Tetas?'
+)
+criaCartao(
+    'Chainsaw man',
+    'qual o nome do demonio que denji enfrenta depois do demonio zumbi',
+    'Demonio do musculo'
+)
+criaCartao(
+    'PALMEIRAS',
+    'Tem mundial?',
+    'não HAHAHAHAHAHA'
+)
+criaCartao(
+    'matematica',
+    'imagine x é igual a uma geladeira e y sejá uma porta, então se elvarmos x a y multiplicado pela hipotenausa vezes 2 é igual a?',
+    'Uma Banana'
+)
+criaCartao(
+    'musica',
+    'Segundo em sua musica, o que crazy frog quer?',
+    'ablimblimblimblomblomblamblam.'
+)
+criaCartao(
+    'sees',
+    'cuuc',
+    'reer'
+)
